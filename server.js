ef53f62beb2221bf8d6742e7e946cf920399626a
Alisha Batham <alishabatham2@gmail.com>
2026-09-15 18:00:31 +0530

    Export app from server.js

--- server.js
+++ server.js
@@ -269,3 +269,5 @@ app.listen(PORT, () => {
   console.log(` Server is running on port ${PORT}`);
   console.log(` API Endpoint: http://localhost:${PORT}/api/consultations`);
 });
+
+export default app;
