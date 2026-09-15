72416db961b5f988e3f3b1d68e0b7a716953bc06
Alisha Batham <alishabatham2@gmail.com>
2026-09-15 16:54:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -260,12 +260,7 @@ app.get('/api/consultations', async (req, res) => {
   }
 });
 
-const frontendPath = path.join(__dirname, "dist");
 
-app.use(express.static(frontendPath));
-app.get("*splat", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-});
 
 // Start express server
 app.listen(PORT, () => {
